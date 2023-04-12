f67362089b254bf0485fc5718683ec2c46b2595e
choigaon1028@gmail.com <choigaon1028@gmail.com>
2023-04-12 22:12:49 +0900

    autoRoute 연결 ("routes/auth")

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -9,9 +10,19 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // The useCreateIndex option has been deprecated for a while and removed as of the Mongoose 6 release
+    // useCreateIndex: true,
   })
   .then(() => console.log("DB Connection Success!"))
   .catch((err) => console.log(err));
+
+/**
+ * 기본적으로 express 서버에서는 JSON을 받는다.
+ */
+app.use(express.json());
+
+// /routes에 만든 route js를 하나씩 임포트 후 연결
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
