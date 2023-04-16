8c2775cf7b949ef8cb15f812e0e848e945eb0c64
choigaon1028@gmail.com <choigaon1028@gmail.com>
2023-04-16 20:24:09 +0900

    유저 로그인 로직 구현

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -23,7 +23,33 @@ router.post("/register", async (req, res) => {
      * async await을 붙이지 않는 경우
      * res.json(user)를 통해 user 객체를 쓰려고 하나, 윗줄이 실행되지 않은 상황. 이를 async await로 맞춰줌(비동기적 실행)
      */
-    res.status(201).json(newUser);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username!");
+
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+
+    const { password, ...info } = user._doc;
+
+    res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
