cd4b88c14018b76c55149af5a61adc5dc42d3cbd
choigaon1028@gmail.com <choigaon1028@gmail.com>
2023-04-16 20:22:17 +0900

    user router 추가

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -14,7 +15,9 @@ mongoose
     // useCreateIndex: true,
   })
   .then(() => console.log("DB Connection Success!"))
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+  });
 
 /**
  * 기본적으로 express 서버에서는 JSON을 받는다.
@@ -23,6 +26,7 @@ app.use(express.json());
 
 // /routes에 만든 route js를 하나씩 임포트 후 연결
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
